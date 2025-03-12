8123ef9ca720e56028c67a577a0b03d67ed7a44c
PhanTrongPhuc <phantrongphuc12004@gmail.com>
2025-03-12 16:13:36 +0700

    Them trang tao moi lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,39 @@
+<template>
+    <div v-if="contact" class="page">
+        <h4>Thêm liên hệ</h4>
+        <ContactForm :contact="contact" @submit:contact="addContact"/>
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            contact: {
+                name: "",
+                phone: "",
+                email: "",
+                address: "",
+                favorite: false,
+            },
+            message: "",
+        };
+    },
+    methods: {
+        async addContact(data) {
+            try {
+                await ContactService.create(data);
+                alert('Liên hệ được thêm thành công.');
+                this.$router.push({ name: "contactbook" });
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+};
+</script>
